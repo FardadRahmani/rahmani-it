27ccaa82c542c04edca5ab835e67e6bd7cd42850
FardadRahmani <rahmanifardad@yahoo.de>
2025-12-22 11:57:26 +0100

    change profile to fit Helpdesk

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -30,7 +30,7 @@ const Hero = () => {
                     initial="hidden"
                     animate="visible"                    
                     className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">
-                        Software Developer
+                        IT Support Specialist
                     </motion.span>
                     
                     <motion.p 
