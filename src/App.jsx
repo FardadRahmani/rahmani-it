24b6f19f1cd50e8d92d8f54723bd9ef0bdb71973
FardadRahmani <rahmanifardad@yahoo.de>
2026-01-06 12:50:56 +0100

    update bg color

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cy300 selection:text-cy900">
       <div className="fixed top-0 -z-10 h-full w-full">
-        <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),
+        <div className="absolute top-0 z-[-2] h-screen w-screen bg-blue-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(30, 64, 175, 0.35),
         rgba(255,255,255,0))]">
 
       </div>
