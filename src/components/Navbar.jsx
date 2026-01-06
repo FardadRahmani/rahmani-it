1ec5f287b84b84f3a1bf56605ff4ad92fa1e911e
FardadRahmani <rahmanifardad@yahoo.de>
2026-01-06 14:01:55 +0100

    change About-Me to Services 3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,9 @@
 import logo from "../assets/fardadLogo.png";
 
 import {FaGithub} from "react-icons/fa";
+/*
 import {FaInstagram} from "react-icons/fa";
-
+*/
 const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
@@ -11,7 +12,7 @@ const Navbar = () => {
         <img className="mx-2 w-10" src={logo} alt="logo"></img>
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <a href="https://github.com/FardadRahmani"><FaGithub /></a>
+        {/* <a href="https://github.com/FardadRahmani"><FaGithub /></a> */}
        
         
       </div>
